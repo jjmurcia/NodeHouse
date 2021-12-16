1b08adb1f01e732e1d24b9b211a58c18674c4c55
Jhon Murcia <jjmurcia.r19@gmail.com>
2021-12-15 20:19:58 -0500

    diseño basico

--- frontend/NodeHouse/src/app/app.module.ts
+++ frontend/NodeHouse/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FooterComponent } from './components/footer/footer.component';
+
+
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { VoidComponent } from './components/void/void.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NavbarComponent,
-    DashboardComponent,
-    FooterComponent,
     HomeComponent,
-    VoidComponent
+    NavbarComponent,
+    VoidComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
