67f2fa6b18e63aa249f6698006961d12961ba3b6
Jhon Murcia <jjmurcia.r19@gmail.com>
2021-12-19 14:27:13 -0500

    ngIf inmueble ngfor

--- frontend/NodeHouse/src/app/components/home/home.component.ts
+++ frontend/NodeHouse/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InmuebleService } from 'src/app/services/inmueble.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  allInmueble: any;
+  typeInmueble: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  inmueble: any = {
+    tipo:"",
+    marca: "",
+    precio: "",
+    stock:"",
+    modelo:""
+  }
+  constructor(private inmuebleService: InmuebleService, private toastr: ToastrService) {
+    this.typeInmueble = ["Acústica","Eléctrica"]
   }
 
+  ngOnInit(): void {
+    this.allInmueble = []
+    this.getAllInmueble()
+  }
+  getAllInmueble(){
+    this.inmuebleService.getAllInmueble().subscribe((Inmueble)=>{
+      this.allInmueble = Inmueble
+    })
+  }
+
+  saveInmueble(): void{
+    if(this.inmueble.tipo && this.inmueble.marca && this.inmueble.precio && this.inmueble.modelo && this.inmueble.stock){
+      this.inmuebleService.postCreateInmueble(this.inmueble).subscribe((respuesta)=>{
+        if((respuesta as any).type=="error"){
+          this.toastr.error((respuesta as any).msg, 'Error!');
+        }else{
+          document.getElementById("closeModal")?.click()
+          this.toastr.success((respuesta as any).msg, 'Bien!');
+          this.getAllInmueble()
+        }
+      })
+    }else{
+      this.toastr.error('Faltan campos por llenar', 'Error!');
+    }
+  }
 }
